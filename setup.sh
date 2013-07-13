761a635d7acb31adcd8e18749b409b6fbef2cbec
MarcoBox <marcoboxdev@gmail.com>
2013-07-13 20:58:15 +0000

    Updated Setup and added file noip

--- setup.sh
+++ setup.sh
@@ -2,10 +2,24 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
+function lnDot {
+	[[ -e "${1}" ]] || return
+	if [[ -L "${1}" ]]; then
+		ln -sf "dotfiles/${1}" $2
+	elif [[ -f "${1}" ]]; then
+		ln -sb "dotfiles/${1}" $2
+	elif [[ -d "${1}/" ]]; then
+		mv "${1}" "${1}~"
+		ln -sf "dotfiles/${1}" $2
+	fi
+}
+# Install realpath that is used in most of our scripts
+sudo apt-get install -y realpath
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git-core
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
@@ -26,18 +40,24 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
-# git pull and install dotfiles as well
+git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
 fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
+git clone https://github.com/marcobox/dotfiles.git
+lnDot .screenrc .
+lnDot .bash_profile .
+lnDot .bashrc .
+lnDot .bashrc_custom .
+lnDot .tmux.conf
+lnDot .tmux.conf.debug
+lnDot .vimrc .
+lnDot .tmux .
+lnDot .emacs.d .
+lnDot .ssh .
+lnDot .terminfo .
+lnDot bin .
 
+sudo cp -b setup/noip /etc/init.d/
+sudo chown root:root /etc/init.d/noip
